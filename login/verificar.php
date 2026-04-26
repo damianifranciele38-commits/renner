e0f1e5c2c137f7e2ad6582f7eba7f72fb70e19a0
damianifranciele38-commits <damianifranciele38@gmail.com>
2026-04-25 21:42:01 -0300

    Refactor verificar.php for cleaner code
    Removed unnecessary whitespace and comments.

--- login/verificar.php
+++ login/verificar.php
@@ -1,10 +1,8 @@
-<?php 
+<?php
 $pdo = new PDO("sqlite:/var/www/html/login/db.db");
-header("Content-type: text/html; charset=utf-8");
 
+// aqui você faria algo com o banco
 
 header('Location: https://www.realizesolucoesfinanceiras.com.br/cartoes-renner/login');
-
+exit;
 ?>
-<html>
-</html>
